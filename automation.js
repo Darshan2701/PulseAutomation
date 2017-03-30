27892f85b486cd50bb950d6d1837c396de0e08ea
DarshanRamesh <darshansoneplus@gmail.com>
2017-03-30 21:53:25 +0530

    wait has been added suuccessfully

--- automation.js
+++ automation.js
@@ -1,15 +1,77 @@
-var Nightmare = require('nightmare');
-var nightmare = Nightmare({ show: true });
+
+// var Nightmare = require('nightmare');
+// var nightmare = Nightmare({ show: true, width: 1280,
+//             height: 800 ,
+//           electronPath: require('./node_modules/electron')});
+
+
+// nightmare
+
+//   .goto('https://pulse.lnttechservices.com')
+//   .type('.login_area input[type="text"]','20073031')
+//   .type('.login_area input[type="password"]','newuseR%123')
+//   .wait(6000)
+  
+//   .click('#QTP_LoginButton')
+//   .wait('.PageBody')
+//   .wait(function(){
+//     console.log("in wait function");
+//   })
+//   .click('#QTP_Logout')
+//   .exists('[#objectnav #Nav_Container #DivID0]')
+  
+//   .then(function(result) {
+
+//         if(result)
+//         {
+//             console.log('Logged in');
+//             return nightmare; //now break out into a function to run the loop
+
+//         } else
+//         {
+//             console.log('OK Button does not exists');
+//             return nightmare;
+
+//         }
+
+//     })
+    
+//   .catch(function (error) {
+//     console.error('Search failed:', error);
+//   });
+
+// // .click('input#QTP_LoginButton')
+// // .wait(3000)
+// //   .click('a#LOCK_Timesheet')
+// //   .click('div#SubDivID10')
+// //   .end()
+// //     .then(function (result) {
+// //       console.log(result)
+// //     })
+// //     .catch(function (error) {
+// //       console.error('Error:', error);
+// //     });
+   
+let Nightmare = require('nightmare');
+let nightmare = Nightmare({
+  electronPath: require('./node_modules/electron'),
+  show: true,
+});
 
 nightmare
-
   .goto('https://pulse.lnttechservices.com')
   .type('.login_area input[type="text"]','20073031')
   .type('.login_area input[type="password"]','newuseR%123')
   .click('#QTP_LoginButton')
-  .wait('.PageBody')
-  
-
+  .wait(1500,'#home #navbar #logout #logtext a')
+  .evaluate(function () {
+    return document.querySelector('#home #navbar #logout #logtext a').href;
+  })
+  .wait(1500)
+  .click('#home #objectnav #Nav_Container #Nav_Content #oSubmenu2 #SubDivID10 #LOCK_Current_Week')
+  .evaluate(function(){
+    return document.querySelector('#home #objectnav #Nav_Container #Nav_Content #oSubmenu2 #SubDivID10 ').href
+  })
   .end()
   .then(function (result) {
     console.log(result);
@@ -17,6 +79,3 @@ nightmare
   .catch(function (error) {
     console.error('Search failed:', error);
   });
-
-
-   
