71e16f9a6cec754c8ca2895456fae348edcf6544
Darhsan Srirangachar Ramesh <dsrirang@cisco.com>
2017-03-31 12:48:18 +0530

    Clicking on current week is fething correctly.

--- automation.js
+++ automation.js
@@ -52,26 +52,40 @@
 // //       console.error('Error:', error);
 // //     });
    
-let Nightmare = require('nightmare');
-let nightmare = Nightmare({
-  electronPath: require('./node_modules/electron'),
+var Nightmare = require('nightmare');
+var nightmare = Nightmare({
+  // electronPath: require('./node_modules/electron'),
   show: true,
 });
 
 nightmare
   .goto('https://pulse.lnttechservices.com')
   .type('.login_area input[type="text"]','20073031')
-  .type('.login_area input[type="password"]','12%123')
+  .type('.login_area input[type="password"]','newuseR%123')
   .click('#QTP_LoginButton')
   .wait(1500,'#home #navbar #logout #logtext a')
   .evaluate(function () {
     return document.querySelector('#home #navbar #logout #logtext a').href;
   })
-  .wait(1500)
-  .click('#home #objectnav #Nav_Container #Nav_Content #oSubmenu2 #SubDivID10 #LOCK_Current_Week')
+
+  //Click the timesheet tab to get the dropdown
+   .wait(1000,'#home #objectnav #Nav_Container #Nav_Content #DivID1 #LOCK_Timesheet #oMenuArrow1') //no need to wait i guess as the DOM has already been loaded. Just trying
+  .click('#home #objectnav #Nav_Container #Nav_Content #DivID1 #LOCK_Timesheet #oMenuArrow1')
+  .wait(1000)
+  //.wait(5000,'#home #objectnav #Nav_Container #Nav_Content #DivID1 #LOCK_Timesheet #oMenuArrow1')
   .evaluate(function(){
-    return document.querySelector('#home #objectnav #Nav_Container #Nav_Content #oSubmenu2 #SubDivID10 ').href
+    return document.querySelector('#home #objectnav #Nav_Container #Nav_Content #DivID1 #LOCK_Timesheet #oMenuArrow1').src
   })
+
+  //click the "Current Week" tab to get a new page.
+  .wait(1000,'#home #objectnav #Nav_Container #Nav_Content #oSubMenu2 #SubDivID10')
+  .click('#home #objectnav #Nav_Container #Nav_Content #oSubMenu2 #SubDivID10')
+  .wait(3500,'#home #objectnav #Nav_Container #Nav_Content #oSubMenu2 #SubDivID10')
+  .evaluate(function() {
+    return document.querySelector('#home #objectnav #Nav_Container #Nav_Content #oSubMenu2 #SubDivID10').className;
+  })
+  .type('.ListDiv .ValueRightTS input[type=value]','9.00')
+
   .end()
   .then(function (result) {
     console.log(result);
